1b15c55ff3166fd64481b6fe1b97dec8c66a8b74
confident-processor-8379 <lancejcasey@gmail.com>
2019-04-05 17:40:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function logShout(string) {
 function logWhisper(string) {
     console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === lowercase) {
+    return 'I can\'t hear you!'
+  } else if(string.toUpperCase() === uppercase) {
+    
+  }
+}
